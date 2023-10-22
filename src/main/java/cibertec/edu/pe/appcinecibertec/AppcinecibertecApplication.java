4bf5098b8b23f8aa257ca9ed9c804fb6298d536f
chiszo <gigibethandrea@gmail.com>
2023-10-22 09:47:28 -0500

    crear repo, clases, y agregar dependecias

---
+++ src/main/java/cibertec/edu/pe/appcinecibertec/AppcinecibertecApplication.java
@@ -0,0 +1,13 @@
+package cibertec.edu.pe.appcinecibertec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppcinecibertecApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppcinecibertecApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cibertec/edu/pe/appcinecibertec/AppcinecibertecApplicationTests.java
@@ -0,0 +1,13 @@
+package cibertec.edu.pe.appcinecibertec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppcinecibertecApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
