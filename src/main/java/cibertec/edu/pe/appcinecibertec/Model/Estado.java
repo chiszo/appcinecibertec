5d84a4946d7919bf20c7dc0ed034881ad9c7e89f
chiszo <gigibethandrea@gmail.com>
2023-10-22 10:57:15 -0500

    agregar métodos CRUD así como conexión con la BD

---
+++ src/main/java/cibertec/edu/pe/appcinecibertec/Model/Estado.java
@@ -0,0 +1,18 @@
+package cibertec.edu.pe.appcinecibertec.Model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name="estado")
+@Data
+
+public class Estado {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer idestado;
+    @Column(name ="descestado")
+    private String descestado;
+}
---
+++ src/main/java/cibertec/edu/pe/appcinecibertec/Repository/EstadoRepository.java
@@ -0,0 +1,10 @@
+package cibertec.edu.pe.appcinecibertec.Repository;
+
+import cibertec.edu.pe.appcinecibertec.Model.Estado;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EstadoRepository extends JpaRepository<Estado, Integer> {
+
+}
