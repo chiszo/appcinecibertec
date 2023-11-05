6fbb87f443f11a1d1071c0f6d01e83a30afd66e4
chiszo <gigibethandrea@gmail.com>
2023-11-05 09:47:37 -0500

    agregar js al modal actualizar

--- src/main/resources/static/script/web/administracion/frmestado.js
+++ src/main/resources/static/script/web/administracion/frmestado.js
@@ -1,4 +1,16 @@
 $(document).on("click","#btnnuevo",function(){
     //alert("Hola :D");
     $("#modalestado").modal("show");
+    $("#txtdescripcion").val("");
+    $("#hddidestado").val("");
+});
+
+$(document).on("click",".btneliminar",function(){
+    $("#modalestado").modal("show");
+});
+
+$(document).on("click",".btnactualizar",function(){
+    $("#modalestado").modal("show");
+    $("#txtdescripcion").val($(this).attr("data-descestado"));
+    $("#hddidestado").val($(this).attr("data-idestado"));
 });
